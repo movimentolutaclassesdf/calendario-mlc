00db57ce70f05e9e311d2f15febf6037c34b3e48
Brandon Dias <dias.desenvolvimento@gmail.com>
2025-11-10 12:13:43 -0300

    nova agenda de atividades e cursos

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // URL base do calendário do Google com ambos os calendários
-const baseURL = "https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FSao_Paulo&src=892c75608373b5dad6199a07949f0fa8e09f8b4041885220f081ce03de9d050c%40group.calendar.google.com&color=%23F6BF26&src=bd2d794a96abb5b4708c08e2efad8136c5663b9ba346e7d4597a7e48fc506435%40group.calendar.google.com&color=%23E67C73&showTitle=0&showTabs=0&showPrint=0&showCalendars=0&showTz=0";
+const baseURL = "https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FSao_Paulo"
+  + "&src=892c75608373b5dad6199a07949f0fa8e09f8b4041885220f081ce03de9d050c%40group.calendar.google.com&color=%23F6BF26"   // amarelo
+  + "&src=bd2d794a96abb5b4708c08e2efad8136c5663b9ba346e7d4597a7e48fc506435%40group.calendar.google.com&color=%23E67C73"   // vermelho
+  + "&src=167c0e7f60a4ecdfbf2dcba6d71dab6c3541fec1af014bd7515a6757861b5533%40group.calendar.google.com&color=%234285F4"   // azul espacial
+  + "&showTitle=0&showTabs=0&showPrint=0&showCalendars=0&showTz=0";
+
 
 // Estado atual da visualização
 let currentMode = 'AGENDA';
